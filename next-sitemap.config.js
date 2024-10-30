d898f82a92c5c3221f73052da07ad3c11c8d5fdb
yasuhikasa <yasuhikasa@yahoo.co.jp>
2024-10-30 18:33:31 +0900

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://nshinri.net', // あなたのサイトのURL
-  generateRobotsTxt: true, // robots.txt も生成します
+  generateRobotsTxt: true, // robots.txtの自動生成
+  sitemapSize: 7000, // サイトマップのページ数制限（必要に応じて）
+  changefreq: 'daily', // サイトの更新頻度
+  priority: 0.7, // ページの優先度
 };
