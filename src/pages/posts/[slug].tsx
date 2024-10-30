ee3a96300ebd167111103b5b3898cb9d7722ea9d
yasuhikasa <yasuhikasa@yahoo.co.jp>
2024-10-30 21:43:58 +0900

    Update [slug].tsx

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -23,7 +23,7 @@ const Post = ({ content, title, date, description, slug }: PostProps) => {
         <title>{title} - 心理カウンセリングコラム</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content={slug} />
+        <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content="/me.png" />
         <meta property="og:url" content={`https://nshinri.net/posts/${slug}`} />
