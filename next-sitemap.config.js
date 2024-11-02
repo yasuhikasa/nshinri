a60c40ec9416b23b78ac13a854187f5ad0f8b077
yasuhikasa <yasuhikasa@yahoo.co.jp>
2024-11-02 11:52:57 +0900

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,22 @@
+import path from 'path';
+import fs from 'fs';
+
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://nshinri.net', // あなたのサイトのURL
-  generateRobotsTxt: true, // robots.txtの自動生成
-  sitemapSize: 7000, // サイトマップのページ数制限（必要に応じて）
-  changefreq: 'daily', // サイトの更新頻度
-  priority: 0.7, // ページの優先度
+const config = {
+  siteUrl: 'https://nshinri.net',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+  additionalPaths: async () => {  // `config` パラメータを削除
+    const postsDirectory = path.join(process.cwd(), 'public', 'posts');
+    const filenames = fs.readdirSync(postsDirectory);
+
+    return filenames.map((filename) => ({
+      loc: `/posts/${filename.replace('.md', '')}`,
+      lastmod: new Date().toISOString(),
+    }));
+  },
 };
+
+export default config;
