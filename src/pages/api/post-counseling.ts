4694fa785851f10ab12b9893e336fd07f246651f
yasuhikasa <yasuhikasa@yahoo.co.jp>
2025-02-16 16:35:44 +0900

    Update post-counseling.ts

--- src/pages/api/post-counseling.ts
+++ src/pages/api/post-counseling.ts
@@ -51,7 +51,7 @@ ${counselingContent.link}
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         tweetText,
-        mediaUrl: counselingContent.image,
+        // mediaUrl: counselingContent.image,
       }),
     });
 
